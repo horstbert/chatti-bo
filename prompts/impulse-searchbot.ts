e896ee712e5109ea15858b47b342964b7433d418
horstbert <74247624+horstbert@users.noreply.github.com>
2023-04-18 14:26:21 +0200

    Create impulse-searchbot.ts

---
+++ prompts/impulse-searchbot.ts
@@ -0,0 +1,7 @@
+export const prompt = `Beziehe deine Antworten ausschließlich auf Inhalte, die du unter https://impulse.de gefunden hast. Beziehe immer mehrere Artikel ein und gebe dir entsprechenden Quellen als URL aus.`;
+
+export const samplePhrases = [
+  "Was ist eine A1 Bescheinigung?",
+  "Was kann ich tun, wenn ein Mitarbeiter stinkt?",
+  "Meine Angestellten trinken zuviel Kaffe und rauchen den ganzen Tag?",
+];
