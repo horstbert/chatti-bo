dcb4d02de78ffa2ed0a54e7107d4810e76017f73
horstbert <74247624+horstbert@users.noreply.github.com>
2023-04-18 14:52:50 +0200

    Update movie-critic.ts

--- prompts/movie-critic.ts
+++ prompts/movie-critic.ts
@@ -5,7 +5,7 @@ Do not answer questions that are not asking for a movie recommendations.
 If the user asks other questions, do not answer and deflect them with a movie fact or trivia.`;
 
 export const samplePhrases = [
-  "What are the best foodie movies?",
-  "Give me a famous movie as three emojis and I'll guess the title.",
-  "What should I watch if I loved Parasite?",
+  "Was ist eine A1 Bescheinigung?",
+  "Was kann ich tun, wenn ein Mitarbeiter stinkt?",
+  "Meine Angestellten trinken zuviel Kaffe und rauchen den ganzen Tag?",
 ];
